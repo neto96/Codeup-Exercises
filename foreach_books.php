c7db277577a43dac2aab2cd066609c242d947d26
Ernesto <neto_neto@hotmail.com>
2016-07-05 15:08:26 -0500

    Add foreach books exercise

---
+++ foreach_books.php
@@ -0,0 +1,31 @@
+<?php
+
+$books = [
+    'The Hobbit' => array(
+        'published' => 1937,
+        'author' => 'J. R. R. Tolkien',
+        'pages' => 310
+    ),
+    'Game of Thrones' => array(
+        'published' => 1996,
+        'author' => 'George R. R. Martin',
+        'pages' => 835
+    ),
+    'The Catcher in the Rye' => array(
+        'published' => 1951,
+        'author' => 'J. D. Salinger',
+        'pages' => 220
+    ),
+    'A Tale of Two Cities' => array(
+        'published' => 1859,
+        'author' => 'Charles Dickens',
+        'pages' => 544
+    )
+];
+
+foreach ($books as $key => $book) {
+	echo "{$key}\n";
+	foreach ($book as $key2 => $info) {
+	echo"{$key2}: {$info}\n";
+	}
+}
