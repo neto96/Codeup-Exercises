034c0d27c85a33eb6b64c9df55745a1945ae85b0
Ernesto <neto_neto@hotmail.com>
2016-07-05 15:07:48 -0500

    Modify foreach to echo every value including ones in nested arrays

--- foreach.php
+++ foreach.php
@@ -2,9 +2,21 @@
 
 $things = ['Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11];
 
-
-foreach ($things as $key => $value) {
-	if (is_scalar($value)) {
-		echo "{$value} is either a float, integer, string or boolean \n";
+foreach ($things as $value) {
+	if (is_int($value)) {
+		echo "{$value}\n";
+	} elseif (is_float($value)) {
+		echo "{$value}\n";
+	} elseif (is_bool($value)) {
+		echo "{$value}\n";
+	} elseif (is_array($value)) {
+		echo"{$value}\n";
+		foreach ($value as $value2) {
+			echo"{$value2}\n";
+		}
+	} elseif (is_null($value)) {
+		echo "{$value}\n";
+	} elseif (is_string($value)) {
+		echo "{$value}\n";
 	}
-}
+};
