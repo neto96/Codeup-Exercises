83e9af399a11f2e6758db424cdf0dd02851ab018
Ernesto <neto_neto@hotmail.com>
2016-07-05 15:59:09 -0500

    Corrected last part of foreach exercise

--- foreach.php
+++ foreach.php
@@ -3,20 +3,13 @@
 $things = ['Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11];
 
 foreach ($things as $value) {
-	if (is_int($value)) {
-		echo "{$value}\n";
-	} elseif (is_float($value)) {
-		echo "{$value}\n";
-	} elseif (is_bool($value)) {
-		echo "{$value}\n";
-	} elseif (is_array($value)) {
-		echo"{$value}\n";
+	if (is_array($value)) {
+		echo "Array ( ";
 		foreach ($value as $value2) {
-			echo"{$value2}\n";
+			echo"{$value2} ";
 		}
-	} elseif (is_null($value)) {
-		echo "{$value}\n";
-	} elseif (is_string($value)) {
-		echo "{$value}\n";
+		echo ")\n";
+	} else {
+		echo $value . PHP_EOL;
 	}
 };
