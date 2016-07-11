c4ff25776953b662e5f4f300959584b178a6da1a
Ernesto <neto_neto@hotmail.com>
2016-07-11 11:06:03 -0500

    Updated to sort by first name in alphabetical order

--- list_with_commas.php
+++ list_with_commas.php
@@ -3,16 +3,22 @@
 function humanizedList ($array) {
 	$lastItem = array_pop($array);
 	array_push($array, " and $lastItem");
-	return $array = implode(",", $array);
+	return $array = implode(", ", $array);
 }
 
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
-
-$physicistsArray = explode(",", $physicistsString);
+$physicistsArray = explode(", ", $physicistsString);
+// $names = [];
 // foreach ($physicistsArray as $physicist) {
-	
+// 	$name = explode(" ", $physicist);
+// 	$names[]= [
+// 		'first name' => $name[0],
+// 		'last name' => $name[1],
+// 	];
 // }
+// var_dump($names);
 
+sort($physicistsArray);
 $famousFakePhysicists = humanizedList($physicistsArray);
 
 echo "Some of the most famous fictional theoretical physicist are {$famousFakePhysicists}.\n";
