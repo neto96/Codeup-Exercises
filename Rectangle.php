ab56b21753202548934b56c889dbf71622f9c09f
Ernesto <neto_neto@hotmail.com>
2016-08-04 15:49:08 -0500

    Class files for Inheritance exercise

---
+++ Rectangle.php
@@ -0,0 +1,17 @@
+<?php
+
+class Rectangle
+{
+    public $width;
+    public $height;
+
+    public function __construct($width, $height) {
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    public function area() {
+        return $this->width * $this->height . PHP_EOL;
+    }
+
+}
